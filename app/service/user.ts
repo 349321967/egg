25118fdcc088ccf7a444e2ea845bf36472ab22cc
youkanzhao <youkanzhao@163.com>
2019-10-24 11:39:37 +0800

    添加自定义sql查询

--- app/service/user.ts
+++ app/service/user.ts
@@ -1,8 +1,14 @@
 import { Service } from 'egg';
+import { QueryTypes } from 'sequelize';
 
 export default class UserService extends Service {
     public async list() {
         const userList = await this.ctx.model.User.findAll({ limit: 10 });
+
+        //自定义sql
+        const dataList = await this.ctx.model.query("select * from users", { type : QueryTypes.SELECT });
+        console.info(dataList);
+
         return userList;
     }
 }
